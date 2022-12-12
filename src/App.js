1cddaf7f1129f30777f4f341fd6ead9df59b2a33
Haci11 <46707887+Haci11@users.noreply.github.com>
2022-12-12 15:25:44 +0100

    Fixing comp-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
+import Header from "./components/Header/Header";
+import Searchbar from "./components/Searchbar/Searchbar";
+import Usercard from "./components/Usercard/Usercard";
 import "./App.css";
 
 function App() {
-    return <div className='App'></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Searchbar />
+      <Usercard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Usercard/Usercard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Usercard.css";
+
+const Usercard = () => {
+  return <div>Usercard</div>;
+};
+
+export default Usercard;
