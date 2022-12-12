caf46f8e870c40f2e428b7331754f3b70f67a2eb
Rikard Johansson <Rikard.com@hotmail.com>
2022-12-12 19:32:46 +0100

    comp-2: Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Header.css";
+import moon from "../../assets/icon-moon.svg";
+// import sun from "../../assets/icon-sun.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+    return (
+        <div className='container'>
+            <h1>devfinder</h1>
+            <button id='mode-btn' className='mode-btn-container'>
+                <p>DARK</p>
+                <img src={moon} alt='mode-icon' />
+            </button>
+        </div>
+    );
 };
 
 export default Header;
