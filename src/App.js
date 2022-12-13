cb9627fcd04383d06e6939aaf9e7d78f82c040a2
Haci11 <46707887+Haci11@users.noreply.github.com>
2022-12-13 21:49:49 +0100

    added functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Header from "./components/Header/Header";
 import Searchbar from "./components/Searchbar/Searchbar";
 import Usercard from "./components/Usercard/Usercard";
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [data, saveData] = useState({});
+
   return (
     <div className="App">
       <Header />
-      <Searchbar />
-      <Usercard />
+      <Searchbar saveData={saveData} />
+      <Usercard data={data} />
     </div>
   );
 }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,18 +2,53 @@ import { useState, useEffect } from "react";
 import img from "../../assets/icon-search.svg";
 import "./Searchbar.css";
 
-const Searchbar = () => {
+const Searchbar = ({ saveData }) => {
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      await fetchUserData("octocat");
+    }
+    fetchData();
+  }, []);
+
+  async function fetchUserData(username) {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
+
+    if (data.message === "Not Found") {
+      setError(true);
+    } else {
+      setError(false);
+      console.log(data);
+      saveData(data);
+      setUsername("");
+    }
+  }
+
+  function handleChange(e) {
+    setUsername(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetchUserData(username);
+  }
   return (
-    <form className="searchbar-container">
+    <form className="searchbar-container" onSubmit={handleSubmit}>
       <img className="searchbar-icon" src={img} alt="" />
       <input
         type="text"
         className="searchbar-input"
         placeholder="Search GitHub username"
         name="username"
+        value={username}
+        onChange={handleChange}
       />
-      {/* <span className="searchbar-error">No results</span> */}
-      <button className="searchbar-btn">Search</button>
+      {error && <span className="searchbar-error">No results</span>}
+      <button type="submit" className="searchbar-btn">
+        Search
+      </button>
     </form>
   );
 };
