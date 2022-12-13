93a0c29cae9d93e99470a04c03e2beafbd9113f1
Haci11 <46707887+Haci11@users.noreply.github.com>
2022-12-13 13:56:25 +0100

    Added style for comp-3

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import img from "../../assets/icon-search.svg";
 import "./Searchbar.css";
 
 const Searchbar = () => {
-  return <div>Searchbar</div>;
+  return (
+    <form className="searchbar-container">
+      <img className="searchbar-icon" src={img} alt="" />
+      <input
+        type="text"
+        className="searchbar-input"
+        placeholder="Search GitHub username"
+        name="username"
+      />
+      {/* <span className="searchbar-error">No results</span> */}
+      <button className="searchbar-btn">Search</button>
+    </form>
+  );
 };
 
 export default Searchbar;
