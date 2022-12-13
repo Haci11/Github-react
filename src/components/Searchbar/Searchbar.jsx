d26fdab7f1afa34a09301d56c8522dc53a834756
Haci11 <46707887+Haci11@users.noreply.github.com>
2022-12-13 22:04:51 +0100

    comp-3: Searchbar component (#6)
    * Added style for comp-3
    
    * added functionality

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,8 +1,56 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import img from "../../assets/icon-search.svg";
 import "./Searchbar.css";
 
-const Searchbar = () => {
-  return <div>Searchbar</div>;
+const Searchbar = ({ saveData }) => {
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      await fetchUserData("octocat");
+    }
+    fetchData();
+  }, []);
+
+  async function fetchUserData(username) {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
+
+    if (data.message === "Not Found") {
+      setError(true);
+    } else {
+      setError(false);
+      console.log(data);
+      saveData(data);
+      setUsername("");
+    }
+  }
+
+  function handleChange(e) {
+    setUsername(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetchUserData(username);
+  }
+  return (
+    <form className="searchbar-container" onSubmit={handleSubmit}>
+      <img className="searchbar-icon" src={img} alt="" />
+      <input
+        type="text"
+        className="searchbar-input"
+        placeholder="Search GitHub username"
+        name="username"
+        value={username}
+        onChange={handleChange}
+      />
+      {error && <span className="searchbar-error">No results</span>}
+      <button type="submit" className="searchbar-btn">
+        Search
+      </button>
+    </form>
+  );
 };
 
 export default Searchbar;
