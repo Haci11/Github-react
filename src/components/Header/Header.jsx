6e16490cfd26dd6e0dbe6ddf7e98027d1f123421
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2022-12-13 22:02:24 +0100

    comp-2: Header component (#5)
    * comp-2: Header component
    
    * comp-2: Header component
    
    Added a toggle button for switching between dark and light mode. Also styled the header to improve the overall aesthetic of the app.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Header.css";
+import moon from "../../assets/icon-moon.svg";
+import sun from "../../assets/icon-sun.svg";
+import useDarkMode from "../ToggleMode/useDarkMode";
 
 const Header = () => {
-  return <div>Header</div>;
+    const [darkMode, toggleDarkMode] = useDarkMode();
+
+    return (
+        <div className='container'>
+            <h1>devfinder</h1>
+            <button
+                id='mode-btn'
+                className='mode-btn'
+                onClick={() => toggleDarkMode()}>
+                <p>{darkMode ? "Dark" : "Light"}</p>
+                <img
+                    src={darkMode ? moon : sun}
+                    alt='mode-icon'
+                    className='mode-icon'
+                />
+            </button>
+        </div>
+    );
 };
 
 export default Header;
